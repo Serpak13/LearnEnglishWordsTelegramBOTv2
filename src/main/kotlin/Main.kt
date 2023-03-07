64c2c6400636639788549abcbf7a97acd2c2b1fa
Sergei Safonov <susafonov@yandex.ru>
2023-03-07 23:42:44 +0100

    Stage-1-2-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,17 +17,16 @@ fun main() {
 
 
         //СТАРТ МЕНЮ
-        println("Меню: 2 - Статистика, 0 - Выход")
-        println("Выберите нужный пункт меню")
-        val userInput = readLine()!!.toInt()
-        when (userInput) {
+        println("Меню: 1 - Учить слова, 2 - Статистика, 0 - Выход")
+
+        when (readLine()?.toInt()) {
             2 -> {  val dictionaryFilter = dictionary.filter{ //Фильтруем для выученных слов
                 it.correctAnswerCount >= 3
             }
+                val totalQuantityWords = dictionary.size
                 val quantityWordsCorrect = dictionaryFilter.size //Количество выученных слов
                 val percentageRatio = (100 / dictionary.size) * quantityWordsCorrect //Процентное соотношение
-                println("Количество слов в словаре: ${dictionary.size}\nВыучено слов ${dictionaryFilter.size} " +
-                    "из ${dictionary.size} | $percentageRatio%")}
+                println("Выучено $quantityWordsCorrect из $totalQuantityWords слов | $percentageRatio%")}
             0 -> break
             else -> println("Такого пункта меню не существует, попробуйте ещё раз")
         }
